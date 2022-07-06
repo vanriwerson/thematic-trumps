09a6e2a9fe43b870d5b65982b8e476238ebe3938
Bruno Riwerson Silva <riwerson@gmail.com>
2022-07-06 15:21:46 -0300

    Algum conteúdo para Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react';
 export default class Home extends Component {
   render() {
     return(
-      <div>
-        <h1>Home</h1>
-      </div>
+      <main className="home">
+        <section>
+          <h1>Super Trunfo</h1>
+          <p>Esse jogo fez parte da minha infância (e da de muita gente ^^) e, agora que já estou um pouco mais avançado nos estudos, sinto-me confiante para fazer essa pequena e justa homenagem. Minha própria releitura do joguinho!</p>
+        </section>
+        <section>
+          <h1>Kipo e os Animonstros</h1>
+          <p>Essa série animada de Radford Sechrist e Bill Wolkoff (Disponível na Netflix) é para mim igualmente apaixonante e com uma temática importantíssima abordada de forma leve.</p>
+          <p>Recomendo que acompanhem Kipo mudando o mundo e espero que gostem do jogo</p>
+        </section>
+      </main>
     );
   }
 }
